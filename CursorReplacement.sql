9ef586f71547c991a34d2d0978c4eeb1b7a9f5b8
skn1116 <103758204+skn1116@users.noreply.github.com>
2022-04-15 20:02:03 +0530

    full code added
    to be used for Data engineering to reduce dependency on sql provided DB objects

--- CursorReplacement.sql
+++ CursorReplacement.sql
@@ -8,3 +8,21 @@ DECLARE @employee_table TABLE (
 -- populate employee table
 INSERT @employee_table
 SELECT distinct employee_id FROM SomeTable
+
+-- enumerate the table
+SET @i = 1
+SET @numrows = (SELECT COUNT(*) FROM @employee_table)
+IF @numrows > 0
+    WHILE (@i <= (SELECT MAX(idx) FROM @employee_table))
+    BEGIN
+
+        -- get the next employee primary key
+        SET @employee_id = (SELECT employee_id FROM @employee_table WHERE idx = @i)
+
+--
+        -- do something with this employee
+--
+   
+        -- increment counter for next employee
+        SET @i = @i + 1
+    END
